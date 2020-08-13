b9622f052688b15a0fd76fbad2fabd253aad4d96
zezhongyan <yanzezhong@outlook.com>
2020-08-13 08:52:01 +0800

    新一次的提交

---
+++ src/main/java/com/example/demo/Backup.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.CLASS)
+@Target(ElementType.FIELD)
+@interface NoflectBackup {
+    int num() default 123;
+}
